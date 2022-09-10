4ce950fe01cf4be479080f7910d61186e85c6b2d
andres <adiaz14@live.com>
2022-09-10 14:06:48 -0500

    Adding new comments

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+//Initialize server on port 3000
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
